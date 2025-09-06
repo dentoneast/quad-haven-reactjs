05d7840ec3d54773fe2b0061f3edb939c0776b16
Denton East <dentoneast@gmail.com>
2025-09-06 02:25:55 -0500

    Misc. fixes.

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,7 +1,12 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('metro-config');
 const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+
+// Initialize resolver if it doesn't exist
+if (!config.resolver) {
+  config.resolver = {};
+}
 
 // Add path mapping for the shared package
 config.resolver.alias = {
@@ -11,10 +16,20 @@ config.resolver.alias = {
 };
 
 // Ensure Metro can resolve TypeScript files
+if (!config.resolver.sourceExts) {
+  config.resolver.sourceExts = ['js', 'jsx', 'json'];
+}
 config.resolver.sourceExts.push('ts', 'tsx');
+
+// Add node modules resolution for shared package
+if (!config.resolver.nodeModulesPaths) {
+  config.resolver.nodeModulesPaths = [];
+}
+config.resolver.nodeModulesPaths.push(path.resolve(__dirname, '../shared/node_modules'));
 
 config.watchFolders = [
   path.resolve(__dirname, '..'),
+  path.resolve(__dirname, '../shared'),
   path.resolve(__dirname, '../shared/node_modules'),
 ];
 
